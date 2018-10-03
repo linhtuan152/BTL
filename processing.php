22d96ac4135213d66bbe0dfb515888063bfdc8a4
linhtuan152 <36038789+linhtuan152@users.noreply.github.com>
2018-10-03 07:59:04 +0700

    ....

--- processing.php
+++ processing.php
@@ -8,20 +8,20 @@ if(isset($_POST['lat']) && isset($_POST['long'])){
 
 //Buoc 01: Ket noi Postgres PDO
     try{
-        $conn=new PDO("pgsql:host=localhost:8080;dbname=haiphong",'postgres','12345');
+        $conn=new PDO("pgsql:host=localhost;dbname=btl",'postgres','12345');
     }catch (PDOException $e) {
         echo "Error : " . $e->getMessage() . "<br/>";
         die();
         }
 //Buoc 02: Truy van Database
-    $sql="SELECT adm1_name, ST_AsText(geom) as geom2 FROM vnm_polbndl_adm1_2014_pdc WHERE ST_Contains(ST_SetSRID(geom,4326) , ST_GeometryFromText('POINT({$getLong}  {$getLat})', 4326))";
-    // echo $sql;
-    $stmt=$conn->prepare($sql);
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $stmt->execute();
-    while($row=$stmt->fetch()){
-        echo $row['adm1_name'];
-    }
+    // $sql="SELECT name_2, ST_AsText(geom) as geom2 FROM tinhthanh WHERE ST_Contains(ST_SetSRID(geom,4326) , ST_GeometryFromText('POINT({$getLong}  {$getLat})', 4326))";
+    // // echo $sql;
+    // $stmt=$conn->prepare($sql);
+    // $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    // $stmt->execute();
+    // while($row=$stmt->fetch()){
+    //     echo $row['name_2'];
+    // }
     // echo 'Kekeke';
 //Buoc 03:
 ?>
