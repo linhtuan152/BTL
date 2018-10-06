835271e2839d53ccbf82a8fd73effa6b4badc1c1
linhtuan152 <36038789+linhtuan152@users.noreply.github.com>
2018-10-06 19:24:43 +0700

    connect db

--- processing.php
+++ processing.php
@@ -7,21 +7,33 @@ if(isset($_POST['lat']) && isset($_POST['long'])){
 }
 
 //Buoc 01: Ket noi Postgres PDO
+    $host = 'localhost';
+    $port = '5433';
+    $dbname = 'NamDinh';
+    $user = 'postgres';
+    $password = '12345';
+
+    $conn_str = "pgsql:host=$host port=$port dbname=$dbname user=$user password=$password";
+
     try{
-        $conn=new PDO("pgsql:host=localhost;dbname=btl",'postgres','12345');
+        $conn=new PDO($conn_str);
     }catch (PDOException $e) {
         echo "Error : " . $e->getMessage() . "<br/>";
         die();
         }
-//Buoc 02: Truy van Database
-    // $sql="SELECT name_2, ST_AsText(geom) as geom2 FROM tinhthanh WHERE ST_Contains(ST_SetSRID(geom,4326) , ST_GeometryFromText('POINT({$getLong}  {$getLat})', 4326))";
-    // // echo $sql;
-    // $stmt=$conn->prepare($sql);
-    // $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    // $stmt->execute();
-    // while($row=$stmt->fetch()){
-    //     echo $row['name_2'];
-    // }
+// Buoc 02: Truy van Database
+    $sql="SELECT adm3_name, ST_AsText(geom) as geom2 FROM vnm_polbnda_adm3_2014_pdc WHERE ST_Contains(ST_SetSRID(geom,4326) , ST_GeometryFromText('POINT({$getLong}  {$getLat})', 4326))";
+    // echo $sql;
+    $stmt=$conn->prepare($sql);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    // echo $stmt;
+    // echo 'ok';
+    while($row=$stmt->fetch()){
+        // echo '?';
+        echo $row['adm3_name'];
+        // echo 'hi';
+    }
     // echo 'Kekeke';
 //Buoc 03:
 ?>
